8651d996318b76f28ca03e9d59eec5bed43a0e7c
willzhang43 <xz408@cam.ac.uk>
2020-07-30 08:56:09 +0100

    adding charity stuff

---
+++ src/js/content.js
@@ -0,0 +1,12 @@
+document.getElementById('level_standardexpanded40895amount').value="1";
+document.getElementById('billing_title').value="Mr.";
+document.getElementById('billing_first_namename').value="Name";
+document.getElementById('billing_last_namename').value="Name";
+document.getElementById('billing_addr_street1name').value="Address";
+document.getElementById('billing_addr_cityname').value="Vancouver";
+document.getElementById('billing_addr_state').value="BC";
+document.getElementById('billing_addr_zipname').value="V1A 1A1";
+document.getElementById('donor_email_addressname').value="will@gmail.com";
+document.getElementById('pstep_finish').click();
+//document.getElementById('ProcessForm').submit()
+//chrome.runtime.sendMessage({type: "closeTab"});
